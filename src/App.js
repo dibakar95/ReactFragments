c7ca46f51d62db5f98037eaf2799e762f8d648bb
Dibakar mandal <dibakar.redbulls@gmail.com>
2020-02-10 10:13:51 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./styles.css";
+import FragmentsDemo from './Components/FragementsDemo';
+export default function App() {
+  return (
+    <div className="App">
+      <FragmentsDemo/>
+    </div>
+  );
+}
---
+++ src/Components/FragementsDemo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function FragementsDemo() {
+return (
+  <React.Fragment>
+    <p>Hello World</p>
+    </React.Fragment>
+)
+}
+export default FragementsDemo;
